fe11eaf53456225d03347746a82f75a29e800852
Ashish Kumar Shukla <ashishk.shukla@impetus.co.in>
2020-05-06 20:33:34 +0530

    cicular array roatation hackerrank

---
+++ src/main/java/ds/array/CircularArrayRoatationHackerRank.java
@@ -0,0 +1,38 @@
+package ds.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CircularArrayRoatationHackerRank {
+	public static void main(String[] args) {
+
+	}
+
+	static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+		int out[] = new int[queries.length];
+		int n = a.length;
+		int finalRotate = k % n;
+		if (finalRotate != 0) {
+			List<Integer> tempList = new ArrayList<Integer>();
+			for (int j = n - 1; j >= n - finalRotate; j--) {
+				tempList.add(a[j]);
+			}
+			for (int j = n - 1; j >= finalRotate - 1; j--) {
+				if ((j - finalRotate) >= 0)
+					a[j] = a[j - finalRotate];
+			}
+			int index = 0;
+			for (int i = tempList.size() - 1; i >= 0; i--) {
+				a[index] = tempList.get(i);
+				index++;
+			}
+		}
+
+		for (int i = 0; i < queries.length; i++) {
+			out[i] = a[queries[i]];
+		}
+
+		return out;
+
+	}
+}
